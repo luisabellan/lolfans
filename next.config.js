65f6558b1398cdaf2decf53aa55f57ce8db42909
Luis Abellan <luis.abellan.dev@gmail.com>
2023-04-15 18:06:24 +0200

    install tRPC and TSLint

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-const withTwin = require('./withTwin.js')
+const withTwin = require("./withTwin.js");
 
 /**
  * @type {import('next').NextConfig}
  */
 module.exports = withTwin({
   reactStrictMode: true,
-})
+  /** We run eslint as a separate task in CI */
+  eslint: { ignoreDuringBuilds: !!process.env.CI },
+});
