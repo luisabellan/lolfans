c59a1190455e05bec3913169fb6b5135e1a9956b
Luis Abellan <luis.abellan.dev@gmail.com>
2023-04-17 05:35:59 +0200

    save

---
+++ prisma/migrations/20230417033455_game_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" TEXT NOT NULL,
+    "description" TEXT,
+    "image" TEXT,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "Game_pkey" PRIMARY KEY ("id")
+);
