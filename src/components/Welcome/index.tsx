1375a4b1e0bc9e1b8ff57e9150bd19980f64518b
Luis Abellan <luis.abellan.dev@gmail.com>
2023-04-17 02:01:14 +0200

    save

--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import tw, { styled } from "twin.macro";
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
 
 interface HeaderProps {
   isUserLogged: boolean;
@@ -18,71 +18,20 @@ const Container = styled.div`
   `}
 `;
 
-
 const Welcome: React.FC<HeaderProps> = ({
   isUserLogged,
   username,
 }: HeaderProps): JSX.Element => {
-  
-
-
-// 384F-AE00
   return (
     <Container>
-
-      
       <h1>Welcome to My Game App</h1>
       {isUserLogged ? (
         <p>Hello, {username}!</p>
       ) : (
         <p>Please login to see your profile</p>
       )}
-
     </Container>
   );
 };
-
-/* const Welcome: React.FC<HeaderProps> = ({
-  isUserLogged,
-  username,
-}: HeaderProps): JSX.Element => {
-  
-  const { data: session } = useSession()
-
-
-  return (
-    <Container>
-
-      
-      <h1>Welcome to My Game App</h1>
-      {isUserLogged ? (
-        <p>Hello, {username}!</p>
-      ) : (
-        <p>Please login to see your profile</p>
-      )}
-
-    </Container>
-  );
-}; */
-
-/* const Welcome: React.FC<HeaderProps> = ({
-  isUserLogged,
-  username,
-}: HeaderProps): JSX.Element => {
-
-  const { data: session } = useSession()
-  
-  if(session) {
-    return <>
-      Signed in as {session.user?.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
-  }
-  return <>
-  Not signed in <br/>
-  <button onClick={() => signIn()}>Sign in</button>
-  </>
-} */
-
 
 export default Welcome;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,7 +10,7 @@ export default NextAuth({
     EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
+        port: process.env.NEXT_PUBLIC_EMAIL_SERVER_PORT,
         auth: {
           user: process.env.EMAIL_SERVER_USER,
           pass: process.env.EMAIL_SERVER_PASSWORD,
