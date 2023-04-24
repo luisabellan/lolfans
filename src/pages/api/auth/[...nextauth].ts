e9e4b3db45677fa144afbbe738b3817b72e8d9d8
Luis Abellan <luis.abellan.dev@gmail.com>
2023-04-24 21:10:31 +0200

    save

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,52 +1,51 @@
 import NextAuth from 'next-auth';
-import { NextAuthOptions } from 'next-auth/types';
+import { NextApiHandler } from 'next';
+import Providers from 'next-auth/providers';
 
-import EmailProvider from 'next-auth/providers/email';
-import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient();
-
-export const authOptions: NextAuthOptions = {
-  /* pages: {
-    signIn: '/',
-    signOut: '/',
-    error: '/',
-    verifyRequest: '/',
-  }, */
-  
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, {
   providers: [
-    EmailProvider({
+    Providers.Email({
       server: {
-        host: process.env.NEXT_PUBLIC_EMAIL_SERVER_HOST!,
-        port: parseInt(process.env.NEXT_PUBLIC_EMAIL_SERVER_PORT!),
+        host: process.env.NEXT_PUBLIC_EMAIL_SERVER_HOST,
+        EMAIL_SERVER_HOST,
+        port: Number(process.env.NEXT_PUBLIC_EMAIL_SERVER_PORT),
         auth: {
-          user: process.env.NEXT_PUBLIC_EMAIL_SERVER_USER!,
-          pass: process.env.NEXT_PUBLIC_EMAIL_SERVER_PASSWORD!,
-        },
+          user: process.env.NEXT_PUBLIC_EMAIL_SERVER_USER,
+          pass: process.env.NEXT_PUBLIC_EMAIL_SERVER_PASSWORD
+        }
       },
-      from: process.env.NEXT_PUBLIC_EMAIL_FROM!,
-      maxAge: 10 * 60, // Magic links are valid for 10 min only
+      from: process.env.NEXT_PUBLIC_EMAIL_SERVER_FROM
     }),
-    
-    GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
-      // redirectUri: process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI!, // check if this is only neededd
-    }),
+    Providers.Google({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET
+    })
   ],
-  theme: {
-    colorScheme: "light",
+  secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60 // 30 days
+  },
+  pages: {
+    signIn: '/auth/signin',
+    signOut: '/auth/signout',
+    error: '/auth/error', // Error code passed in query string as ?error=
+    verifyRequest: '/auth/verify-request', // (used for check email message)
+    newUser: null // If set, new users will be directed here on first sign in
   },
   callbacks: {
-    async jwt({token}) {
-      token.userRole = "admin"
-      return token
+    async jwt(token, user, account, profile, isNewUser) {
+      // Add access_token to the token right after signin
+      if (account?.accessToken) {
+        token.accessToken = account.accessToken;
+      }
+      return token;
     },
+    async session(session, token) {
+      session.accessToken = token.accessToken;
+      return session;
+    }
   },
-  
-  adapter: PrismaAdapter(prisma),
-}
+});
 
-export default NextAuth(authOptions);
+export default authHandler;
