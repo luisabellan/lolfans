d405afa4f7daca468dffbb15209229e263b28716
Luis Abellan <luis.abellan.dev@gmail.com>
2023-05-01 23:41:10 +0200

    summoners page - save

--- src/pages/players.tsx
+++ src/pages/players.tsx
@@ -1,18 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
-import axios from "axios";
+import { SetStateAction, useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
 import HeaderMenu from "@/components/HeaderMenu";
 import tw, { styled } from "twin.macro";
 import { useAtom } from "jotai";
-import {isUserLoggedAtom} from "@/atoms/store";
+import { isUserLoggedAtom } from "@/atoms/store";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { match } from "assert";
 
 const Main = tw.main`flex flex-col justify-between items-center`;
 const Button = tw.button`bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`;
 const TextInput = tw(Field)`px-4 py-2 border rounded w-full my-2`;
 const ErrorText = tw.span`text-red-500`;
+const Header = tw.h2`text-3xl`;
+const SummonerInfo = tw.h2`flex flex-col items-start self-center  pt-20`;
 
 interface PlayerInfo {
   id: string;
@@ -33,26 +36,34 @@ const Players = (props: Props): JSX.Element => {
   const [summonerIcon, setSummonerIcon] = useState("");
   const RIOT_API_KEY = props.apiKey;
   const [loggedIn, setLoggedIn] = useAtom(isUserLoggedAtom);
+  const [matches, setMatches] = useState([])
+  const [matchesIDs, setMatchesIDs] = useState([{}])
 
-  const handleSubmit = async(values: any) => {
-    const summonerById = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${values.summonerName}?api_key=${RIOT_API_KEY}`;
-    
-    axios
-    .get(summonerById)
-    .then((res) => {
+
+  const handleSubmit = async (values: any) => {
+    try {
+      const summonerById = `https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${values.summonerName}?api_key=${RIOT_API_KEY}`;
+      const res = await axios.get(summonerById);
       setPlayerInfo(res.data);
-    })
-    .catch((e) => {
+  
+      const summonerIconUrl = `https://ddragon.leagueoflegends.com/cdn/13.8.1/img/profileicon/${res.data.profileIconId}.png`;
+      setSummonerIcon(summonerIconUrl);
+  
+      const matchesIDsUrl = `https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/${res.data.puuid}/ids?start=0&count=20&api_key=${RIOT_API_KEY}`;
+      const matchesIDsRes = await axios.get(matchesIDsUrl);
+      const myMatchesUrls = matchesIDsRes.data.map(
+        (matchID: string) => `https://europe.api.riotgames.com/lol/match/v5/matches/${matchID}?api_key=${RIOT_API_KEY}`
+      );
+  
+      const matchesRes = await Promise.all(myMatchesUrls.map((url: string) => axios.get(url)));
+      const myMatchesData = matchesRes.map((res: AxiosResponse<any>) => res.data);
+      setMatches([...myMatchesData]);
+    } catch (e) {
       console.log(e);
-      alert("Summoner not found");
-    });
-    
-    await setSummonerIcon(`https://ddragon.leagueoflegends.com/cdn/13.8.1/img/profileicon/${playerInfo.profileIconId}.png`);
-      
+      // alert("Summoner not found");
+    }
   };
-
-
- 
+  
 
   const playerSchema = Yup.object().shape({
     summonerName: Yup.string()
@@ -93,17 +104,21 @@ const Players = (props: Props): JSX.Element => {
           )}
         </Formik>
 
-        <div>
+        <SummonerInfo tw=" ">
           {playerInfo.name && (
-            <>
-              <p>{playerInfo.name}</p>
-              <Image width={50} height={50} src={summonerIcon} alt={"Profile Icon"} />
-              <p>Lvl: {playerInfo.summonerLevel}</p>
-              <p>KDA: {playerInfo.summonerLevel}</p>
-              <p>Division: {playerInfo.summonerLevel}</p>
-            </>
+            <div tw="flex flex-col items-center">
+              <Header tw="text-4xl text-center">{playerInfo.name}</Header>
+              <Image width={200} height={200} src={summonerIcon} alt={"Profile Icon"} />
+              <div tw="self-start pt-10">
+                <p tw="self-start text-2xl">Lvl: {playerInfo.summonerLevel}</p>
+                <p tw="self-start text-2xl">Division: {playerInfo.summonerLevel}</p>
+
+              </div>
+              <Header tw="text-4xl text-center pt-20">Last 10 matches</Header>
+              {console.log(matchesIDs)}
+            </div>
           )}
-        </div>
+        </SummonerInfo>
       </Main>
     </>
   );
